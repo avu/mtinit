d2dbe64e7dc788f38e8943b3ac2013b065220691
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2014-03-12 18:53:20 +0400

    imports

--- android/app/src/main/java/com/intellij/mobiusrss/ChooseSourceActivity.java
+++ android/app/src/main/java/com/intellij/mobiusrss/ChooseSourceActivity.java
@@ -1,15 +1,25 @@
 package com.intellij.mobiusrss;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.Arrays;
 import java.util.List;
+
+import nl.matshofman.saxrssreader.RssFeed;
 
 public class ChooseSourceActivity extends Activity {
 
